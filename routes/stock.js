34b099fb2d804bde662d664d7180f044a23693bc
Thotatb <tanavthota@gmail.com>
2025-05-12 18:12:51 -0400

    adding untracked files

---
+++ routes/stock.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { portfolioData } from '../data/index.js';
+const router = Router();
+import { verifyString } from '../utils/auth/user_data.js';
+
+router.route('/:stockTicker').get(async (req, res) => {
+    try {
+        req.params.stockTicker = verifyString(req.params.stockTicker, "stockTicker")
+    } catch (e) {
+        const errorCode = e[0];
+        return res.status(errorCode).render('error', {
+            errorCode: errorCode,
+            title: `${errorCode} Error`,
+            errorMessage: e[1],
+        });
+    }
+    return res.status(200).render('stock', {isLoggedIn: true, title: req.params.stockTicker})
+})
+
+export default router
