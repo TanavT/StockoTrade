a0d46be79ea28ecebdcb8f61a4ab9e317109630c
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-05-12 20:20:07 -0400

    Reset button bug fix

--- public/js/reset_button.js
+++ public/js/reset_button.js
@@ -1,9 +1,10 @@
-function confirmWarning() {
-    if(confirm("Are you sure you want to reset your portfolio? All data will be reset.")){
-        alert("Portfolio Reset")
-    } else {
-        alert("Reset request cancelled.")
-    }
+const resetButton = document.getElementById('reset_button');
+if (resetButton) {
+	resetButton.addEventListener('click', (event) => {
+		if (confirm('Are you sure you want to reset your portfolio? All data will be reset.')) {
+			alert('Portfolio Reset');
+		} else {
+			alert('Reset request cancelled.');
+		}
+	});
 }
-const resetButton = document.getElementById("reset_button")
-resetButton.onclick = confirmWarning();
