1db684aa8fb78734e075274ced42eb7e0a93d193
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-05-13 20:09:35 -0400

    fix reset button

--- public/js/reset_button.js
+++ public/js/reset_button.js
@@ -1,12 +1,14 @@
 const resetButton = document.getElementById('reset_button');
 if (resetButton) {
-  resetButton.addEventListener('click', (event) => {
-	const ok = confirm('Are you sure you want to reset your portfolio? All data will be reset.');
-    if (!ok) {
-      event.preventDefault();
-      alert('Reset request cancelled.');
-    } else {
-		alert('Portfolio Reset');
-	}
-  });
+	resetButton.addEventListener('click', (event) => {
+		const ok = confirm(
+			'Are you sure you want to reset your portfolio? All data will be reset.'
+		);
+		if (!ok) {
+			event.preventDefault();
+			alert('Reset request cancelled.');
+		} else {
+			alert('Portfolio Reset');
+		}
+	});
 }
